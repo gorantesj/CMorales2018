e2c6c3a6acad8a01286e8202714308b0a9135801
Déborah <deborahandrea.m@hotmail.com>
2021-04-16 18:59:24 -0500

    merge

--- R/encuestadores.R
+++ R/encuestadores.R
@@ -17,12 +17,61 @@ bd <-  bind_rows(equipo1, equipo2, equipo3, equipo4)
 bd <-bd %>% set_names(codigos$id)
 
 rm(equipo1, equipo2, equipo3, equipo4)
+# limpiar bd --------------------------------------------------------------
+#Fecha y día
+bd<- bd %>%  mutate(fecha = gsub(pattern  ="GMT-5", fixed = T,
+                                 replacement = "", x =fecha ),
+                    fecha = gsub(pattern  ="m.", fixed = T,
+                                 replacement = "", x =fecha ),
+                    fecha = gsub(pattern  =" a.", fixed = T,
+                                 replacement = "", x =fecha ),
+                    fecha = str_squish(fecha))
+
+bd <-bd %>%  mutate(fecha= ymd_hms(fecha)) %>%
+  mutate(dia = floor_date(fecha, unit = "day"))
+
+bd %>%  count(dia)
+
+bd %<>%   filter(dia != as.Date( "2021-04-14 00:00:00"))
+
+#
+#
+# quitar otro mensaje
+bd %<>%  mutate(credencial = gsub(pattern = " (Agradecer y solicitar la participación de otra persona)",
+                                  replacement = "",
+                                  x = credencial,
+                                  fixed = T))
+
+
+# bd  %>%
+#   map(~{sub(pattern = '*.[[:punct:]]',replacement =  '\\1',  x = .x) %>%
+#       gsub(pattern = " (No leer)",replacement =  "", fixed = T, x = .) %>%
+#       str_squish( .) %>%
+#       gsub(pattern = "9 No sabe / No contesta",
+#            replacement =  "No sabe / No contesta", fixed = T, x = .)}) %>%
+#   as_tibble()
+
 
 #Encuestas por persona
 bd %>%  count(nombre_encuestador) %>%  arrange(desc(n))   %>%  mutate(mean(n))
 
 
 # pct de encuestas a favor de Carlos
-bd %>% group_by(nombre_encuestador) %>%  count(municipal) %>%
+bd %>% group_by(equipo, nombre_encuestador) %>%  count(municipal) %>%
   mutate(n = round(n *100/sum(n))) %>%  filter(municipal== "Carlos Morales, por MORENA") %>%
   arrange(desc(n))
+  # ungroup() %>%
+  # mutate(mean(n))
+
+bd %>%  group_by(nombre_encuestador) %>%
+  arrange(fecha) %>%
+  mutate(lag =lag(fecha), lag2 = fecha-lag) %>%
+  # select(nombre_encuestador, lag, fecha, lag2) %>%
+  mutate(duracion = as.numeric(lag2)) %>%
+  filter(duracion > 60) %>%  count( equipo, nombre_encuestador, seccion, manzana, personas) %>%
+  arrange(desc(n)) %>%
+  view()
+
+bd %>% filter()
+arrange(fecha) %>%  select(fecha, equipo, nombre_encuestador, seccion, manzana) %>%
+  view()
--- R/procesamiento.R
+++ R/procesamiento.R
@@ -3,61 +3,87 @@ library(tidyverse)
 library(survey)
 library(readr)
 library(lubridate)
+library(magrittr)
+library(readxl)
 
-# leer bases 
-equipo1 <- read_csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Clientes/Chiapas/Tuxtla-2021/Bds/TUXTLA GUTIÉRREZ, EQUIPO 1.csv")
-equipo2 <- read_csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Clientes/Chiapas/Tuxtla-2021/Bds/TUXTLA GUTIÉRREZ, EQUIPO 2.csv")
-equipo3 <- read_csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Clientes/Chiapas/Tuxtla-2021/Bds/TUXTLA GUTIÉRREZ, EQUIPO 3.csv")
-equipo4 <- read_csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Clientes/Chiapas/Tuxtla-2021/Bds/TUXTLA GUTIÉRREZ, EQUIPO 4.csv")
+# leer bases
+equipo1 <- read_csv("data/TUXTLA GUTIÉRREZ, EQUIPO 1.csv")
+equipo2 <- read_csv("data/TUXTLA GUTIÉRREZ, EQUIPO 2.csv")
+equipo3 <- read_csv("data/TUXTLA GUTIÉRREZ, EQUIPO 3.csv")
+equipo4 <- read_csv("data/TUXTLA GUTIÉRREZ, EQUIPO 4.csv")
 
-codigos <- read_csv("~/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Clientes/Chiapas/Tuxtla-2021/Bds/codigos.csv")
+codigos <- read_csv("data/codigos.csv")
 
 bd <-  bind_rows(equipo1, equipo2, equipo3, equipo4)
 
 bd <-bd %>% set_names(codigos$id)
 
+rm(equipo1, equipo2, equipo3, equipo4)
+
+# INE
+edad_ine <- read_csv("data/INE/grupos_etarios.csv")
+
+# Limpar lista nominal ----------------------------------------------------
+edad_ine %<>%  filter(NOMBRE_ENTIDAD == "CHIAPAS", NOMBRE_MUNICIPIO == "TUXTLA GUTIERREZ")
+
+edad <-edad_ine %>%
+  gather(grupo_nominal, nominal, c(LISTA_18_HOMBRES:LISTA_65_Y_MAS_MUJERES)) %>%
+  # gather(grupo_padron, padron, c(PADRON_18_HOMBRES:PADRON_65_Y_MAS_MUJERES)) %>%
+  select(SECCION, grupo_nominal, nominal)
+
+edad %<>%  mutate(grupo_nominal = gsub(pattern = "_HOMBRES", replacement = "", grupo_nominal),
+                  grupo_nominal = gsub(pattern = "_MUJERES", replacement = "", grupo_nominal))
+
+edad %<>%  mutate(
+  edad_nominal = case_when(
+    grupo_nominal %in% c( "LISTA_18", "LISTA_19", "LISTA_20_24") ~   "18 a 24 años",
+    grupo_nominal %in% c( "LISTA_25_29", "LISTA_30_34") ~    "25 a 34 años",
+    grupo_nominal %in% c( "LISTA_35_39", "LISTA_40_44") ~   "35 a 44 años",
+    grupo_nominal %in% c( "LISTA_45_49", "LISTA_50_54") ~  "45 a 54 años",
+    grupo_nominal %in% c( "LISTA_55_59", "LISTA_60_64") ~   "55 a 64 años",
+    grupo_nominal == "LISTA_65_Y_MAS" ~   "65 años o más" ) ) %>%
+  group_by(SECCION ,edad_nominal) %>%  summarise(n =sum(nominal)) %>%  ungroup()
 
 # limpiar bd --------------------------------------------------------------
+#Fecha y día
 
-bd<- bd %>%  mutate(fecha = gsub(pattern  ="GMT-5", fixed = T,
-                            replacement = "", x =fecha ),
-               fecha = gsub(pattern  ="m.", fixed = T,
-                            replacement = "", x =fecha ),
-               fecha = gsub(pattern  =" a.", fixed = T,
-                            replacement = "", x =fecha ),
-               fecha = str_squish(fecha))
+bd %<>%  mutate(auxiliar = grepl(pattern = "p", x = fecha),
+  fecha = gsub(pattern  ="GMT-5", fixed = T, replacement = "", x =fecha  ) %>%
+    ymd_hms() + hours(12)*auxiliar)
 
-# Reporte de levantamiento ------------------------------------------------
 
-# encuestas
-bd <-bd %>%  mutate(fecha= ymd_hms(fecha)) %>%  
-  mutate(dia = floor_date(fecha, unit = "day")) 
 
-bd %>%  count(dia) 
+bd <-bd %>%  mutate(fecha= ymd_hms(fecha)) %>%
+  mutate(dia = floor_date(fecha, unit = "day"))
+
+bd %>%  count(dia)
+
 bd %<>%   filter(dia != as.Date( "2021-04-14 00:00:00"))
-bd %>%   filter(dia == as.Date( "2021-04-16 00:00:00")) %>%  count(nombre_encuestador)
 
-bd   %<>% 
-  mutate (municipal  = sub('*.[[:punct:]]', '\\1', municipal) ,
-          municipal = str_squish(municipal))
-
-bd %>%  count(seccion, manzana) %>%  arrange(desc(n)) %>%  count(n)
-
-bd   %>%   count(municipal)  %>%  
-  mutate(n = round(n *100/sum(n))) %>% 
-  arrange(desc(n))
-
-bd  %>% group_by(equipo, nombre_encuestador) %>% 
-  count(municipal)  %>%  
-  mutate(n = round(n *100/sum(n))) %>% 
-  arrange(desc(n)) 
-  
+#
+#
+# quitar otro mensaje
+bd %<>%  mutate(credencial = gsub(pattern = " (Agradecer y solicitar la participación de otra persona)",
+                                  replacement = "",
+                                  x = credencial,
+                                  fixed = T))
 
 
+bd  %<>%
+  map_df(~{if(is.character(.x)){
+    sub(pattern = '*.[[:punct:]]',replacement =  '\\1',  x = .x) %>%
+  gsub(pattern = " (No leer)",replacement =  "", fixed = T, x = .) %>%
+  str_squish( .) %>%
+  gsub(pattern = "9 No sabe / No contesta",
+       replacement =  "No sabe / No contesta", fixed = T, x = .) }
+    else(.x)})
+
+bd %>%  group_by(nombre_encuestador) %>%
+  arrange(fecha) %>%
+  mutate(lag =lag(fecha), lag2 = fecha-lag) %>%
+  # select(nombre_encuestador, lag, fecha, lag2) %>%
+  mutate(duracion = as.numeric(lag2)) %>%
+  filter(duracion > 60)
 
 
-bd  %>% group_by(equipo) %>% 
-  count(municipal)  %>%  
-  mutate(n = round(n *100/sum(n))) %>% 
-  filter(municipal %in% c("Carlos Morales, por MORENA","Willy Ochoa, por PRI, PAN y PRD",
-                          "Paco Rojas, por Movimiento Ciudadano")) 
+bd %>%  count(municipal) %>%  mutate(n = round(n*100/sum(n))) %>%  arrange(desc(n))
